2e11a1e122e55b033164b33298ea6e94e38bc3ef
Garrett Legg <s25glegg@kimberly.edu>
2022-10-06 16:26:49 +0000

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+
+    <div class="min-h-screen bg-gray-100">
+        <header class="flex items-center justify-between px-10 py-4 bg-white shadow-xl">
+            <div class="flex flex-col items-center">
+                <a href="/">
+                    <div>
+                        <img class="w-12" src="{{ asset('img/k.svg') }}">
+                        Photo Of The Game
+                </a>
+            </div>
+            <div>
+                <div class="font-bold">
+                    <div>
+                        @if (Route::has('login'))
+                            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                                @auth
+                                            <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                                @else
+                                    <a href="{{ route('login') }}"
+                                        class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                                    @if (Route::has('register'))
+                                        <a href="{{ route('register') }}"
+                                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                    @endif
+                                @endauth
+                            </div>
+                        @endif
+                    </div>
+                    <a> user avatar</a>
+                </div>
+            </div>
+        </header>
+
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
+</html>
